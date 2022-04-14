e11e2aa1754607b02899b6dab41689163194588a
Rozakiin <6567067+Rozakiin@users.noreply.github.com>
2022-04-14 12:30:05 +0100

    Add assests with updated user to post text

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$("#add_post").submit(function (event) {
+    alert("Data Inserted Successfully!");
+})
+
+$("#update_post").submit(function (event) {
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function (n, i) {
+        data[n['name']] = n['value']
+    })
+
+
+    var request = {
+        "url": `http://localhost:3000/api/posts/${data.id}`,
+        "method": "PUT",
+        "data": data
+    }
+
+    $.ajax(request).done(function (response) {
+        alert("Data Updated Successfully!");
+    })
+
+})
+
+function deletefunction(ids){   
+
+    var request = {
+        "url" : `http://localhost:3000/api/posts/${ids}`,
+        "method" : "DELETE"
+    }
+    if(confirm("Do you really want to delete this record?")){
+        $.ajax(request).done(function(response){
+            alert("Data Deleted Successfully!");
+            location.reload();
+        })
+    }
+} 
+
