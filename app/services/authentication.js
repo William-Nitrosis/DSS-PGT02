523649c000d19c4a3460fef19d5821dfd825f9c0
Rozakiin-Laptop <6567067+Rozakiin@users.noreply.github.com>
2022-04-19 19:35:52 +0100

    add redirect to original url after auth

--- app/services/authentication.js
+++ app/services/authentication.js
@@ -3,5 +3,6 @@ exports.ensureAuthenticated = (req,res,next) => {
         return next();
     }
     req.flash('error_msg' , 'please login to view this resource');
+    req.session.returnTo = req.originalUrl;
     res.redirect('/users/login');
 }
