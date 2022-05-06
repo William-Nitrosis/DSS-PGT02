ac51a81105b6af3b1e2144e114767565fb503802
Rozakiin-Laptop <6567067+Rozakiin@users.noreply.github.com>
2022-05-06 17:08:30 +0100

    Update Error handling

--- app/services/render.js
+++ app/services/render.js
@@ -6,24 +6,23 @@ exports.posts = (req, res) => {
     // Make a get request to /api/posts for all posts
     axios.get('http://localhost:3000/posts/api/posts')
         .then(function(response){
-            console.log(response);
             res.render('posts', { posts : response.data });
         })
         .catch(err =>{
-            res.send(err);
+            console.log(err);
+            res.status(500).send({ message : "Error Occurred while retriving post information" });
         })
-
-    
 }
+
 exports.add_post = (req,res) => {
     res.render('add_post');
 }
 
 exports.update_post = async (req, res) => {
-
     // check the current user owns the post
     const postid = validator.escape(req.query.id);
     const post = await Post.findById(postid).exec();
+
     if (post.userid === req.user.id) {    
         // Make a get request to /api/posts for post with id
         axios.get('http://localhost:3000/posts/api/posts', { params : { id : postid }})
@@ -31,7 +30,8 @@ exports.update_post = async (req, res) => {
                 res.render("update_post", { post : postdata.data})
             })
             .catch(err =>{
-                res.send(err);
+                console.log(err);
+                res.status(500).send({ message : "Error Occurred while retriving post information" });
             })
     } else {
         console.log("Invalid User, IDs are not equal. Expected:"+post.userid+" Recieved:"+req.user.id);
